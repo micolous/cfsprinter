f6acb1f0d75c6e42956908031574e255345dd5b7
Michael Farrell <micolous+git@gmail.com>
2011-04-25 15:30:41 +0930

    ugly hack for py2exe plugin issues

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ try:
 except ImportError:
 	print "Warning: py2exe is not installed.  (Though it may not be available on your platform.)"
 
-
 requires = ['win32api']
 if version < '2.6.0':
 	requires.append("simplejson")
@@ -15,12 +14,19 @@ setup(
 	version='0.1.1',
 	author='Michael Farrell',
 	url='http://github.com/micolous/cfsprinter',
-	py_modules=['browsersupport', 'mappingsupport', 'pagerscraper', 'pagerprinter', 'plugins', 'plugins.lpdprint', 'plugins.winprint'],
+	py_modules=['browsersupport', 'mappingsupport', 'pagerscraper', 'pagerprinter'],
+	packages=['plugins'],
+	includes=['plugins.winprint', 'plugins.lpdprint'],
 	data_files=[
 		('doc', [
 			'pagerprinter.example.ini',
 			'README.txt',
 			'LICENSE.txt'
+		]),
+		# UGLY HACK
+		('plugins', [
+			'winprint.py',
+			'lpdprint.py'
 		]),
 	],
 	requires=requires,
